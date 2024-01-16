ef75e631bbe420b01d428c0029697b2e55c48b5b
Othmane elkhiari <othmaneelkhiari@gmail.com>
2024-01-16 12:06:01 +0100

    helllo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dbjson = require('./db.json');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+
+const app = express();
+
+app.get('/product', (req, res) => {
+    res.json(dbjson);
+});
+
+app.get('/product/:id', (req, res) => {
+    const id = req.params.id;
+    const product = dbjson.find(p => p.id == id);
+    res.json(product);
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
